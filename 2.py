5e95bd849ad9a57b410876129748991e546a3ba6
Mikail Chughtai <mokhsinovich@mail.ru>
2025-02-23 19:45:07 +0300

    hz

--- 2.py
+++ 2.py
@@ -25,101 +25,55 @@ from itertools import *
 #                 print(p)
 
 
-# a = 'ВЛТУ'
-# c = 1
-# for a1, a2, a3, a4 in product(a, repeat=4):
-#     word = a1 + a2 + a3 + a4
-#     print(c, word)
-#     c += 1
 
-# a = 'ЖИРАФ'
-# c = 0
-# for a1, a2, a3, a4 in product(a, repeat=4):
-#     word = a1 + a2 + a3 + a4
-#     if word.count('Р') == 1:
-#         c += 1
+
+# def f(a, b, c, d):
+#     return c and (a or d) and (d <= b)
 #
-# print(c)
-
-
-# a = '0123456789'
-# c = 0
 #
-# for a1, a2, a3, a4, a5 in product(a, repeat=5):
-#     word1 = a1 + a2 + a3 + a4 + a5
-#     word2 = ''
-#     if word1.count('3') == 2:
-#         for w in word1:
-#             if int(w) % 2 != 0:
-#                 word2 += 'x'
-#             else:
-#                 word2 += w
-#         if not('x2' in word2 or '2x' in word2):
-#             c += 1
-#             print(f'{c}: {word2}')
+# for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
+#     table = [(a1, a2, a3, 0), (a4, 1, 0, a5), (0, a6, 1, 0)]
+#     if len(table) == len(set(table)):
+#         for p in permutations('abcd'):
+#             if [f(**dict(zip(p, r))) for r in table] == [1, 1, 1]:
+#                 print(p)
 
 
-# c = 1
+# def f(w, x, y):
+#     return (x <= y) and (w or (not w))
 #
-# for a1, a2, a3, a4, a5, a6 in product('БКФ', repeat=6):
-#     w = a1 + a2 + a3 + a4 + a5 + a6
-#     print(c, w)
-#     c += 1
-
-
-# a = '0234567'
-# c = 0
 #
-# for i in permutations(a, 5):
-#     x = ''.join(i)
-#     if x[0] != '0':
-#         x = x.replace('7', '1').replace('5', '1').replace('3', '1').replace('6', '0').replace('4', '0').replace('2',                                                                                     '0')
-#         if ('00' not in x) and ('11' not in x):
-#             c += 1
-#
-# print(c)
-
-# s = 101 * '1'
-#
-# while '111' in s:
-#     s = s.replace('111', '22', 1).replace('222', '11', 1)
-#
-# print(s)
-
-
-# s = '4' + 125 * '6' + '4'
-#
-# while '63' in s or '664' in s or '6665' in s:
-#     if '63' in s:
-#         s = s.replace('63', '4', 1)
-#     else:
-#         if '664' in s:
-#             s = s.replace('664', '5', 1)
-#         else:
-#             if '6665' in s:
-#                 s = s.replace('6665', '3', 1)
-#
-# print(s)
-
-
-# m = set()
-#
-# for i in range(2, 1000):
-#     s = i * '8'
-#
-#     while '555' in s or '888' in s:
-#         s = s.replace('555', '8', 1).replace('888', '55', 1)
-#     m.add(s)
-#
-# print(len(m))
+# for a1, a2, a3 in product([0, 1], repeat=3):
+#     table = [(1, 1, 0), (a1, 1, a2), (1, 0, 1), (1, a3, 1)]
+#     if len(table) == len(set(table)):
+#         for p in permutations('wxy'):
+#             if [f(**dict(zip(p, r))) for r in table] == [0, 0, 1, 1]:
+#                 print(p)
 
 
 
-# s = 200 * '5'
-# while '555' in s or '333' in s:
-#     if '555' in s:
-#         s = s.replace('555', '3', 1)
-#     else:
-#         s = s.replace('333', '5', 1)
-#
-# print(sum([int(i) for i in s]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
